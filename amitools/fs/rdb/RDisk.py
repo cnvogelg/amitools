33339a8c86203e5e9b65f53f82b5f0fb8dc7a0b5
Christian Vogelgsang <C.Vogelgsang@web.de>
2012-01-29 19:33:34 +0100

    cleaned up rdisk info output

--- amitools/fs/rdb/RDisk.py
+++ amitools/fs/rdb/RDisk.py
@@ -2,6 +2,7 @@ from ..block.rdb.RDBlock import *
 from ..block.rdb.PartitionBlock import *
 from FileSystem import FileSystem
 from ..blkdev.PartBlockDevice import PartBlockDevice
+import amitools.util.ByteSize as ByteSize
 
 class RDisk:
   def __init__(self, rawblk):
@@ -57,10 +58,20 @@ class RDisk:
   
   def get_info(self):
     res = []
+    # physical disk info
     pd = self.rdb.phy_drv
-    res.append("PhysicalDisk: cyls=%d, heads=%d, secs=%d" % (pd.cyls, pd.heads, pd.secs))
+    total_blks = self.get_total_blocks()
+    total_bytes = self.get_total_bytes()
+    extra="heads=%d sectors=%d" % (pd.heads, pd.secs)
+    res.append("PhysicalDisk:        %8d %8d  %10d  %s  %s" \
+      % (0, pd.cyls-1, total_blks, ByteSize.to_bi_str(total_bytes), extra))
+    # logical disk info
     ld = self.rdb.log_drv
-    res.append("LogicalDisk:  rdb_blks=[%d:%d], cyls=[%d:%d], cyl_blks=%d" % (ld.rdb_blk_lo, ld.rdb_blk_hi, ld.lo_cyl, ld.hi_cyl, ld.cyl_blks))
+    extra="rdb_blks=[%d:%d] cyl_blks=%d" % (ld.rdb_blk_lo, ld.rdb_blk_hi, ld.cyl_blks)
+    logic_blks = self.get_logical_blocks()
+    logic_bytes = self.get_logical_bytes()
+    res.append("LogicalDisk:         %8d %8d  %10d  %s  %s" \
+      % (ld.lo_cyl, ld.hi_cyl, logic_blks, ByteSize.to_bi_str(logic_bytes), extra))
     # add partitions
     num = 0
     for p in self.parts:
@@ -69,18 +80,45 @@ class RDisk:
       extra = ""
       flags = p.flags
       if flags & PartitionBlock.FLAG_BOOTABLE == PartitionBlock.FLAG_BOOTABLE:
-        extra += " bootable"
+        extra += " bootable pri=%d" % de.boot_pri
       if flags & PartitionBlock.FLAG_NO_AUTOMOUNT == PartitionBlock.FLAG_NO_AUTOMOUNT:
         extra += " no_automount"
-      res.append("Partition: #%d %-06s cyls=[%8d:%8d] boot_pri=%d %s" % (num, name, de.low_cyl, de.high_cyl, de.boot_pri, extra))
+      part_blks = self.get_partition_blocks(num)
+      part_bytes = self.get_partition_bytes(num)
+      res.append("Partition: #%d %-06s %8d %8d  %10d  %s %s" \
+        % (num, name, de.low_cyl, de.high_cyl, part_blks, ByteSize.to_bi_str(part_bytes), extra))
       num += 1
     return res
+
+  def get_logical_blocks(self):
+    ld = self.rdb.log_drv
+    cyls = ld.hi_cyl - ld.lo_cyl + 1
+    return cyls * ld.cyl_blks
+
+  def get_logical_bytes(self, block_bytes=512):
+    return self.get_logical_blocks() * block_bytes
+
+  def get_total_blocks(self):
+    pd = self.rdb.phy_drv
+    return pd.cyls * pd.heads * pd.secs
+  
+  def get_total_bytes(self, block_bytes=512):
+    return self.get_total_blocks() * block_bytes
 
   def get_num_partitions(self):
     return len(self.parts)
   
   def get_partition_blkdev(self, num):
     return PartBlockDevice(self.rawblk, self.parts[num])
+
+  def get_partition_blocks(self, num):
+    p = self.parts[num]
+    cyls = p.dos_env.high_cyl - p.dos_env.low_cyl + 1
+    ld = self.rdb.log_drv
+    return cyls * ld.cyl_blks
+
+  def get_partition_bytes(self, num, block_size=512):
+    return self.get_partition_blocks(num) * block_size
 
   def find_partition_by_device_name(self, name):
     lo_name = name.lower()
---
+++ amitools/util/ByteSize.py
@@ -0,0 +1,19 @@
+"""tools to convert byte sizes into strings suitable for disk/file sizes"""
+
+KIB_SIZE = 1024
+MIB_SIZE = KIB_SIZE * 1024
+GIB_SIZE = MIB_SIZE * 1024
+
+def to_bi_str(size):
+  if size < 1000:
+    return "%3Bi" % size
+  else:
+    for unit in "KMGT":
+      next = size / KIB_SIZE
+      if next < 10:
+        rem = size % KIB_SIZE
+        return "%d.%d%si" % (next, rem / 100, unit)
+      elif next < 1000:
+        return "%3d%si" % (next, unit)
+      size = next
+    return "NaNBi"
