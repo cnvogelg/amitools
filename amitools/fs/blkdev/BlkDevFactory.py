7467ac91d660218454ae9c99ce72d817369bd547
Christian Vogelgsang <C.Vogelgsang@web.de>
2012-01-29 20:19:27 +0100

    if image file is not writable then enter read_only mode

--- amitools/fs/blkdev/BlkDevFactory.py
+++ amitools/fs/blkdev/BlkDevFactory.py
@@ -1,9 +1,14 @@
+import os
 from ADFBlockDevice import ADFBlockDevice
 from HDFBlockDevice import HDFBlockDevice
 from RawBlockDevice import RawBlockDevice
 
 class BlkDevFactory:
-  def create(self, img_file, read_only=False):    
+  def create(self, img_file, read_only=False):
+    # check access
+    if not os.access(img_file, os.W_OK):
+      read_only = True
+    # check extension
     ext = img_file.lower()
     if ext.endswith('.adf'):
       return ADFBlockDevice(img_file, read_only)
