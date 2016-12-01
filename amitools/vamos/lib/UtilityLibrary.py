2be4155b4a2ee2808d171c5a7b8db4d1dce22a89
Christian Vogelgsang <chris@vogelgsang.org>
2016-12-01 21:58:21 +0100

    added some tag functions to utility.library

--- amitools/vamos/lib/UtilityLibrary.py
+++ amitools/vamos/lib/UtilityLibrary.py
@@ -1,5 +1,7 @@
 from amitools.vamos.AmigaLibrary import *
 from amitools.vamos.lib.lexec.ExecStruct import LibraryDef
+from amitools.vamos.lib.util.UtilStruct import TagItemDef
+from amitools.vamos.lib.util.TagList import *
 from amitools.vamos.Log import *
 
 class UtilityLibrary(AmigaLibrary):
@@ -63,4 +65,37 @@ class UtilityLibrary(AmigaLibrary):
     else:
       return 0
 
+  # Tags
 
+  def NextTagItem(self, ctx):
+    ti_ptr_addr = ctx.cpu.r_reg(REG_A0)
+    ti_addr = ctx.mem.access.r32(tr_ptr_addr)
+    ti_addr = next_tag_item(ctx, ti_addr)
+    if ti_addr is None:
+      next_addr = 0
+    else:
+      next_addr = ti_addr + 8
+    ctx.mem.access.w32(tr_ptr_addr, next_addr)
+    return ti_addr
+
+  def FindTagItem(self, ctx):
+    tagValue = ctx.cpu.r_reg(REG_D0)
+    ti_addr = ctx.cpu.r_reg(REG_A0)
+    if ti_addr == 0:
+      return 0
+    while True:
+      ti_addr = next_tag_item(ctx, ti_addr)
+      if ti_addr is None:
+        return 0
+      tag, _ = get_tag(ctx, ti_addr)
+      if tag == tagValue:
+        return ti_addr
+      ti_addr += 8
+
+  def GetTagData(self, ctx):
+    defaultValue = ctx.cpu.r_reg(REG_D1)
+    ti_addr = self.FindTagItem(ctx)
+    if ti_addr != 0:
+      return get_tag(ctx, ti_addr)[1]
+    else:
+      return defaultValue
--- amitools/vamos/lib/util/TagList.py
+++ amitools/vamos/lib/util/TagList.py
@@ -1,4 +1,5 @@
 from UtilStruct import *
+from amitools.vamos.AccessStruct import AccessStruct
 
 TAG_DONE = 0
 TAG_IGNORE = 1
@@ -16,7 +17,7 @@ class Tag:
       self.name = tag_info[self.tag]
     else:
       self.name = None
-    
+
   def __str__(self):
     if self.user:
       return "(U:%s/%08x->%08x)" % (self.name, self.tag, self.data)
@@ -27,7 +28,7 @@ class TagList:
   def __init__(self, tag_info):
     self.tags = []
     self.tag_info = tag_info
-    
+
   def add(self, tag, data):
     self.tags.append(Tag(tag,data, self.tag_info))
 
@@ -36,7 +37,7 @@ class TagList:
       if tag.name == tagname:
         return tag
     return None
-  
+
   def __str__(self):
     return "[%s]" % ",".join(map(str, self.tags))
 
@@ -50,10 +51,34 @@ def taglist_parse_tagitem_ptr(mem, addr, tag_info=None):
     elif tag == TAG_IGNORE:
       addr += 8
     elif tag == TAG_SKIP:
-      addr += 8
+      addr += 8 * (1 + data)
     elif tag == TAG_MORE:
       addr = data
     else:
       result.add(tag,data)
       addr += 8
   return result
+
+def next_tag_item(ctx, ti_addr):
+  if ti_addr == 0:
+    return None
+  while True:
+    tag, data = get_tag(ctx, ti_addr)
+    if tag == TAG_DONE:
+      return None
+    elif tag == TAG_MORE:
+      ti_addr = data
+      if data == 0:
+        return None
+    elif tag == TAG_IGNORE:
+      ti_addr += 8
+    elif tag == TAG_SKIP:
+      ti_addr += 8 * (1 + data)
+    else:
+      return ti_addr
+
+def get_tag(ctx, ti_addr):
+  ti = AccessStruct(ctx.mem, TagItemDef, ti_addr)
+  tag = ti.r_s("ti_Tag")
+  data = ti.r_s("ti_Data")
+  return tag, data
