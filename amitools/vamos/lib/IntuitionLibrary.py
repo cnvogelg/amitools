40e8f572fb5291e1bc0eb609e850e61fc0a7969a
githubaf <alexander.fritsch@adtran.com>
2017-11-02 05:45:50 -0500

    added def AutoRequest(self,ctx) -> needed for gcc/libnix programms error-messages like 'Need a 68020 or higher CPU'

--- amitools/vamos/lib/IntuitionLibrary.py
+++ amitools/vamos/lib/IntuitionLibrary.py
@@ -16,3 +16,11 @@ class IntuitionLibrary(AmigaLibrary):
     msg_ptr = ctx.cpu.r_reg(REG_A0)
     msg = ctx.mem.access.r_cstr(msg_ptr)
     log_main.error("-----> DisplayAlert: #%08x - '%s'@%08x <-----", alert_num, msg, msg_ptr)
+
+#selco, 30.Oct.2017
+  def AutoRequest(self,ctx):
+    IntuiText = ctx.cpu.r_reg(REG_A1)
+    IText = ctx.mem.access.r32(IntuiText+12) # IntuiText.ITexT
+    msg = ctx.mem.access.r_cstr(IText)
+    log_main.error("-----> AutoRequest '%s'",msg)
+
