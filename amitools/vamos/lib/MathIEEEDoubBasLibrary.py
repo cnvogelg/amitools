631312025fe75ac44f50b37958e7c70cc6ffcde0
Alexander Fritsch <selco@t-online.de>
2017-11-05 12:13:21 -0500

    IEEEDPFlt() implemented.

--- amitools/vamos/lib/MathIEEEDoubBasLibrary.py
+++ amitools/vamos/lib/MathIEEEDoubBasLibrary.py
@@ -4,6 +4,19 @@ from amitools.vamos.Log import *
 import struct
 import math
 
+#selco
+#taken from http://www.neotitans.com/resources/python/python-unsigned-32bit-value.html
+#Casting Python integers into signed 32-bit equivalents
+def int32(x):
+  if x>0xFFFFFFFF:
+    raise OverflowError
+  if x>0x7FFFFFFF:
+    x=int(0x100000000-x)
+    if x<2147483648:
+      return -x
+    else:
+      return -2147483648
+  return x
 
 def fromDouble(number):
   hi=0
@@ -42,6 +55,18 @@ class MathIEEEDoubBasLibrary(AmigaLibrary):
     (hi,lo)=fromDouble(ctx.cpu.r_reg(REG_D0)+0.0)
     ctx.cpu.w_reg(REG_D1,lo)
     return hi
+
+
+
+#selco
+# name above should have been IEEEDPFlt !???
+  def IEEEDPFlt(self, ctx):
+    integer32bit=int32(ctx.cpu.r_reg(REG_D0));   # we need to cast to 32bit integers here, otherwise negative values are wrong! 
+    (hi,lo)=fromDouble(integer32bit)
+    ctx.cpu.w_reg(REG_D1,lo)
+    return hi
+
+
 
   def IEEEDPCmp(self, ctx):
     arg1=toDouble(ctx.cpu.r_reg(REG_D0),ctx.cpu.r_reg(REG_D1))
