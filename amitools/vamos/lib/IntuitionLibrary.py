6d65833a81d1a622a77ecf23a0929913a601b0ce
Christian Vogelgsang <chris@vogelgsang.org>
2018-03-06 12:56:24 +0100

    cleanup intuition.library

--- amitools/vamos/lib/IntuitionLibrary.py
+++ amitools/vamos/lib/IntuitionLibrary.py
@@ -4,7 +4,7 @@ from amitools.vamos.Log import *
 
 class IntuitionLibrary(AmigaLibrary):
   name = "intuition.library"
-  
+
   def __init__(self, config):
     AmigaLibrary.__init__(self, self.name, LibraryDef, config)
 
@@ -17,17 +17,15 @@ class IntuitionLibrary(AmigaLibrary):
     msg = ctx.mem.access.r_cstr(msg_ptr)
     log_main.error("-----> DisplayAlert: #%08x - '%s'@%08x <-----", alert_num, msg, msg_ptr)
 
-#selco, 30.Oct.2017
   def AutoRequest(self,ctx):
     IntuiText = ctx.cpu.r_reg(REG_A1)
     IText = ctx.mem.access.r32(IntuiText+12) # IntuiText.ITexT
     msg = ctx.mem.access.r_cstr(IText)
     log_main.error("-----> AutoRequest '%s'",msg)
 
-#selco, 17. Nov.2017
   def EasyRequestArgs(self,ctx):
     EasyStruct = ctx.cpu.r_reg(REG_A1)
     es_TextFormat = ctx.mem.access.r32(EasyStruct+12) # EasyStruct.es_TextFormat
     msg = ctx.mem.access.r_cstr(es_TextFormat)
-    log_main.error("-----> AutoRequest '%s'",msg)
+    log_main.error("-----> EasyRequest '%s'",msg)
 
