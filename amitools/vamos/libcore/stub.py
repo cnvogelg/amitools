8520481e3c9a50862682681e6c398ef773bc0c57
Christian Vogelgsang <chris@vogelgsang.org>
2018-03-15 21:27:03 +0100

    added lib stub

---
+++ amitools/vamos/libcore/stub.py
@@ -0,0 +1,208 @@
+from types import MethodType
+import time
+import traceback
+
+from .impl import LibImplScanner
+from amitools.vamos.Exceptions import VamosInternalError
+from amitools.vamos.CPU import *
+
+class LibStub(object):
+  """a lib stub is a frontend object instance that wraps all calls into
+     a library implementation.
+
+     It is suitable for binding these functions to traps of the machine
+     emulation.
+
+     A wrapped call adds exception handler, return value processing
+     or optional profiling features.
+  """
+  def __init__(self, name, impl, fd, profile=None):
+    """create a stub for a given implementation and associated fd description"""
+    self.name = name
+    self.impl = impl
+    self.fd = fd
+    # a table with all methods mapped via index
+    self.func_tab = [None] * fd.get_num_indices()
+    # (optional) attach profile object
+    self.profile = profile
+    self.log_exc = None
+
+  def get_func_tab(self):
+    return self.func_tab
+
+  def _get_callee_pc(self, ctx):
+    """a call stub log helper to extract the callee's pc"""
+    sp = ctx.cpu.r_reg(REG_A7)
+    return ctx.mem.r32(sp)
+
+  def _gen_arg_dump(self, args, ctx):
+    """a call stub helper to dump the registers of a call"""
+    if args == None or len(args) == 0:
+      return ""
+    result = []
+    for a in args:
+      name = a[0]
+      reg = a[1]
+      reg_num = int(reg[1])
+      if reg[0] == 'a':
+        reg_num += 8
+      val = ctx.cpu.r_reg(reg_num)
+      result.append("%s[%s]=%08x" % (name,reg,val))
+    return ", ".join(result)
+
+  def _handle_exc(self):
+    """default exception handler"""
+    traceback.print_exc()
+    raise
+
+
+class LibStubGen(object):
+  """the lib stub generator scans a lib impl and creates stubs for all
+     methods found there"""
+  def __init__(self, exc_handler=None,
+               log_missing=None, log_valid=None,
+               ignore_invalid=True):
+    self.exc_handler = exc_handler
+    self.log_missing = log_missing
+    self.log_valid = log_valid
+    self.ignore_invalid = ignore_invalid
+
+  def gen_stub(self, name, impl, fd, ctx, profile=None):
+    # first scan the impl
+    scanner = LibImplScanner(inc_std_funcs=True)
+    scanner.scan(impl, fd)
+    # raise an error if impl is not valid
+    num_invalid = scanner.get_num_invalid_funcs()
+    num_error = scanner.get_num_error_funcs()
+    if num_invalid > 0 and not self.ignore_invalid:
+      raise VamosInternalError("'%s' impl has %d invalid funcs!" % (name, num_invalid))
+    if num_error > 0:
+      raise VamosInternalError("'%s' impl has %d error funcs!" % (name, num_error))
+
+    # create stub object
+    stub = LibStub(name, impl, fd, profile)
+
+    # generate valid funcs
+    valid_funcs = scanner.get_valid_funcs().values()
+    for fd_func, impl_method in valid_funcs:
+      stub_func = self.wrap_func(fd_func, impl_method, ctx, profile)
+      # bind method to stub instance
+      stub_method = MethodType(stub_func, stub)
+      # store in stub
+      setattr(stub, fd_func.get_name(), stub_method)
+      # add to func tab
+      index = fd_func.get_index()
+      stub.func_tab[index] = stub_method
+
+    # generate missing funcs
+    missing_funcs = scanner.get_missing_funcs().values()
+    for fd_func in missing_funcs:
+      stub_func = self.wrap_missing_func(fd_func, ctx, profile)
+      # bind method to stub instance
+      stub_method = MethodType(stub_func, stub)
+      # store in stub
+      setattr(stub, fd_func.get_name(), stub_method)
+      # add to func tab
+      index = fd_func.get_index()
+      stub.func_tab[index] = stub_method
+
+    # (optional) replace exception handler of stub
+    if self.exc_handler is not None:
+      method = MethodType(self.exc_handler, stub)
+      setattr(stub, "_handle_exc", method)
+
+    # return final stub
+    return stub
+
+  def wrap_missing_func(self, fd_func, ctx, profile):
+    """create a stub func for a missing function in impl
+       returns an unbound method for the stub instance
+    """
+    log = self.log_missing
+    if log is None:
+      # without tracing
+      def stub_func(this, *args, **kwargs):
+        # return d0=0
+        ctx.cpu.w_reg(REG_D0, 0)
+    else:
+      name = fd_func.get_name()
+      func_args = fd_func.get_args()
+      bias = fd_func.get_bias()
+      # with tracing
+      def stub_func(this, *args, **kwargs):
+        callee_pc = this._get_callee_pc(ctx)
+        call_info = "%4d %s( %s ) from PC=%06x" % (bias, name, this._gen_arg_dump(func_args, ctx), callee_pc)
+        log.warn("? CALL: %s -> d0=0 (default)",call_info)
+        ctx.cpu.w_reg(REG_D0, 0)
+    func = stub_func
+
+    # wrap profiling?
+    if profile:
+      index = fd_func.get_index()
+      prof = profile.get_func_prof(index)
+      def profile_func(this, *args, **kwargs):
+        stub_func(this, *args, **kwargs)
+        prof.count(0.0)
+      func = profile_func
+
+    # return created func
+    return func
+
+  def wrap_func(self, fd_func, impl_method, ctx, profile):
+    """create a stub func for a valid impl func
+       returns an unbound method for the stub instaance
+    """
+    def base_func(this, *args, **kwargs):
+      """the base function to call the impl,
+         set return vals, and catch exceptions"""
+      try:
+        res = impl_method(ctx)
+        if res is not None:
+          if type(res) in (list, tuple):
+            ctx.cpu.w_reg(REG_D0, res[0] & 0xffffffff)
+            ctx.cpu.w_reg(REG_D1, res[1] & 0xffffffff)
+          else:
+            ctx.cpu.w_reg(REG_D0, res & 0xffffffff)
+        return res
+      except:
+        this._handle_exc()
+    func = base_func
+
+    # wrap around logging method?
+    log = self.log_valid
+    if log:
+      name = fd_func.get_name()
+      func_args = fd_func.get_args()
+      bias = fd_func.get_bias()
+      def log_func(this, *args, **kwargs):
+        callee_pc = this._get_callee_pc(ctx)
+        call_info = "%4d %s( %s ) from PC=%06x" % (bias, name, this._gen_arg_dump(func_args, ctx), callee_pc)
+        log.info("{ CALL: %s" % call_info)
+        res = base_func(this, *args, **kwargs)
+        if res is not None:
+          if type(res) in (list, tuple):
+            res_str = "d0=%08x, d1=%08x" % res
+          else:
+            res_str = "d0=%08x" % res
+        else:
+          res_str = "n/a"
+        log.info("} CALL: -> %s" % res_str)
+      func = log_func
+
+    # wrap profiling?
+    if profile:
+      index = fd_func.get_index()
+      prof = profile.get_func_prof(index)
+      if log:
+        call = log_func
+      else:
+        call = base_func
+      def profile_func(this, *args, **kwargs):
+        start = time.clock()
+        call(this, *args, **kwargs)
+        end = time.clock()
+        delta = end - start
+        prof.count(delta)
+      func = profile_func
+
+    return func
---
+++ test/unit/libcore_stub.py
@@ -0,0 +1,164 @@
+from __future__ import print_function
+import logging
+import pytest
+
+from amitools.vamos.libcore import LibStubGen, LibCtx
+from amitools.vamos.lib.VamosTestLibrary import VamosTestLibrary
+from amitools.vamos.machine import MockCPU, MockMemory
+from amitools.vamos.libcore import LibProfile
+from amitools.fd import read_lib_fd
+
+def _check_stub(stub):
+  # check func tab
+  func_tab = stub.get_func_tab()
+  for f in func_tab:
+    assert f is not None
+
+def _check_profile(fd, profile):
+  print_hello_func = fd.get_func_by_name('PrintHello')
+  dummy_func = fd.get_func_by_name('Dummy')
+  print_hello_prof = profile.get_func_prof(print_hello_func.get_index())
+  dummy_func_prof = profile.get_func_prof(dummy_func.get_index())
+  assert print_hello_prof.get_calls() == 1
+  assert dummy_func_prof.get_calls() == 1
+  profile.dump()
+
+def _check_log(caplog):
+  assert caplog.record_tuples == [
+    ('valid', logging.INFO, '{ CALL:   30 PrintHello(  ) from PC=000000'),
+    ('valid', logging.INFO, '} CALL: -> d0=00000000'),
+    ('missing', logging.WARN, '? CALL:   54 Dummy( a[d0]=00000000, b[d1]=00000000 ) from PC=000000 -> d0=0 (default)'),
+    ('valid', logging.INFO, '{ CALL:   48 Swap( a[d0]=00000000, b[d1]=00000000 ) from PC=000000'),
+    ('valid', logging.INFO, '} CALL: -> d0=00000000, d1=00000000')
+  ]
+
+def _create_ctx():
+  cpu = MockCPU()
+  mem = MockMemory()
+  return LibCtx(cpu, mem)
+
+def libcore_stub_gen_base_test():
+  name = 'vamostest.library'
+  impl = VamosTestLibrary()
+  fd = read_lib_fd(name)
+  ctx = _create_ctx()
+  # create stub
+  gen = LibStubGen()
+  stub = gen.gen_stub(name, impl, fd, ctx)
+  _check_stub(stub)
+  # call func
+  stub.PrintHello()
+  stub.Dummy()
+  stub.Swap()
+
+def libcore_stub_gen_profile_test():
+  name = 'vamostest.library'
+  impl = VamosTestLibrary()
+  fd = read_lib_fd(name)
+  ctx = _create_ctx()
+  profile = LibProfile(name, fd)
+  # create stub
+  gen = LibStubGen()
+  stub = gen.gen_stub(name, impl, fd, ctx, profile)
+  _check_stub(stub)
+  # call func
+  stub.PrintHello()
+  stub.Dummy()
+  stub.Swap()
+  _check_profile(fd, profile)
+
+def libcore_stub_gen_log_test(caplog):
+  caplog.set_level(logging.INFO)
+  name = 'vamostest.library'
+  impl = VamosTestLibrary()
+  fd = read_lib_fd(name)
+  ctx = _create_ctx()
+  log_missing = logging.getLogger('missing')
+  log_valid = logging.getLogger('valid')
+  # create stub
+  gen = LibStubGen(log_missing=log_missing, log_valid=log_valid)
+  stub = gen.gen_stub(name, impl, fd, ctx)
+  _check_stub(stub)
+  # call func
+  stub.PrintHello()
+  stub.Dummy()
+  stub.Swap()
+  _check_log(caplog)
+
+def libcore_stub_gen_log_profile_test(caplog):
+  caplog.set_level(logging.INFO)
+  name = 'vamostest.library'
+  impl = VamosTestLibrary()
+  fd = read_lib_fd(name)
+  ctx = _create_ctx()
+  log_missing = logging.getLogger('missing')
+  log_valid = logging.getLogger('valid')
+  profile = LibProfile(name, fd)
+  # create stub
+  gen = LibStubGen(log_missing=log_missing, log_valid=log_valid)
+  stub = gen.gen_stub(name, impl, fd, ctx, profile)
+  _check_stub(stub)
+  # call func
+  stub.PrintHello()
+  stub.Dummy()
+  stub.Swap()
+  _check_log(caplog)
+  _check_profile(fd, profile)
+
+def libcore_stub_gen_exc_default_test(capsys):
+  name = 'vamostest.library'
+  impl = VamosTestLibrary()
+  fd = read_lib_fd(name)
+  ctx = _create_ctx()
+  # create stub
+  gen = LibStubGen()
+  stub = gen.gen_stub(name, impl, fd, ctx)
+  _check_stub(stub)
+  # call func
+  ctx.mem.w_cstr(0, 'RuntimeError')
+  with pytest.raises(RuntimeError):
+    stub.RaiseError()
+  # check for traceback
+  captured = capsys.readouterr()
+  lines = captured.err.strip().split('\n')
+  assert lines[0] == 'Traceback (most recent call last):'
+  assert lines[-1] == 'RuntimeError: VamosTest'
+
+def libcore_stub_gen_exc_custom_test(capsys):
+  name = 'vamostest.library'
+  impl = VamosTestLibrary()
+  fd = read_lib_fd(name)
+  ctx = _create_ctx()
+  # create stub
+  def custom_handler(self):
+    print("hello, world!")
+  gen = LibStubGen(exc_handler=custom_handler)
+  stub = gen.gen_stub(name, impl, fd, ctx)
+  _check_stub(stub)
+  # call func
+  ctx.mem.w_cstr(0, 'RuntimeError')
+  stub.RaiseError()
+  # check for traceback
+  captured = capsys.readouterr()
+  lines = captured.out.strip().split('\n')
+  assert lines[-1] == "hello, world!"
+
+def libcore_stub_gen_multi_arg(capsys):
+  caplog.set_level(logging.INFO)
+  name = 'vamostest.library'
+  impl = VamosTestLibrary()
+  fd = read_lib_fd(name)
+  ctx = _create_ctx()
+  log_missing = logging.getLogger('missing')
+  log_valid = logging.getLogger('valid')
+  profile = LibProfile(name, fd)
+  # create stub
+  gen = LibStubGen(log_missing=log_missing, log_valid=log_valid)
+  stub = gen.gen_stub(name, impl, fd, ctx, profile)
+  _check_stub(stub)
+  # call func
+  stub.PrintHello(1, 2, a=3)
+  stub.Dummy(3, b='hello')
+  stub.Swap('hugo', None, c=3)
+  _check_log(caplog)
+  _check_profile(fd, profile)
