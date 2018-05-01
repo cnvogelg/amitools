7d946076d7a1c7283266d9492796d035529e2534
Christian Vogelgsang <chris@vogelgsang.org>
2018-05-01 22:44:22 +0200

    added dos_loadseg test

---
+++ test/src/dos_loadseg.c
@@ -0,0 +1,21 @@
+#include <dos/dos.h>
+#include <proto/dos.h>
+
+int main(int argc, char *argv[])
+{
+  BPTR seglist;
+
+  if(argc != 2) {
+    Printf("Usage: %s <file>\n", argv[0]);
+    return 1;
+  }
+
+  seglist = LoadSeg(argv[1]);
+  if(seglist == 0) {
+    Printf("No seglist found: %s\n", argv[1]);
+    return 2;
+  }
+
+  UnLoadSeg(seglist);
+  return 0;
+}
