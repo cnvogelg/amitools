c5d66e16730defa142834264ec41884aea98f1b3
Christian Vogelgsang <chris@vogelgsang.org>
2018-06-04 22:14:50 +0200

    allow nesting of config values

--- amitools/vamos/cfgcore/value.py
+++ amitools/vamos/cfgcore/value.py
@@ -19,9 +19,43 @@ def parse_scalar(val_type, val, allow_none=False):
   return val_type(val)
 
 
+def split_nest(in_str, sep=',', nest_pair='()', keep_nest=False):
+  """split a string by sep, but don't touch seps in nested pairs"""
+  res = []
+  cur = []
+  nesting = 0
+  if nest_pair is None:
+    nest_begin = None
+    nest_end = None
+  else:
+    nest_begin = nest_pair[0]
+    nest_end = nest_pair[1]
+  # split string loop
+  for c in in_str:
+    if c == nest_end:
+      nesting -= 1
+    if nesting == 0:
+      if c in sep:
+        if len(cur) > 0:
+          res.append("".join(cur))
+          cur = []
+      elif c not in (nest_begin, nest_end) or keep_nest:
+        cur.append(c)
+    else:
+      cur.append(c)
+    # adjust nesting
+    if c == nest_begin:
+      nesting += 1
+  # final element
+  if len(cur) > 0:
+    res.append("".join(cur))
+  return res
+
+
 class Value(object):
   def __init__(self, item_type, default=None, allow_none=None):
     self.item_type = item_type
+    self.nest_pair = None
     if allow_none is None:
       self.allow_none = item_type is str
     else:
@@ -43,9 +77,16 @@ class Value(object):
 
 
 class ValueList(object):
-  def __init__(self, item_type, default=None, allow_none=None, sep=','):
+  def __init__(self, item_type, default=None, allow_none=None,
+               sep=',', nest_pair='()'):
     self.item_type = item_type
     self.sep = sep
+    self.nest_pair = nest_pair
+    self.is_sub_value = type(item_type) in (Value, ValueList, ValueDict)
+    if self.is_sub_value:
+      self.sub_nest_pair = item_type.nest_pair
+    else:
+      self.sub_nest_pair = None
     if allow_none is None:
       self.allow_none = item_type is str
     else:
@@ -60,13 +101,16 @@ class ValueList(object):
       return []
     elif type(val) is str:
       # split string by sep
-      val = val.split(self.sep)
+      val = split_nest(val, self.sep, self.sub_nest_pair)
     elif type(val) not in (list, tuple):
       raise ValueError("expected list or tuple: %s" % val)
     # rebuild list
     res = []
     for v in val:
-      r = parse_scalar(self.item_type, v, self.allow_none)
+      if self.is_sub_value:
+        r = self.item_type.parse(v)
+      else:
+        r = parse_scalar(self.item_type, v, self.allow_none)
       res.append(r)
     return res
 
@@ -82,10 +126,16 @@ class ValueList(object):
 
 class ValueDict(object):
   def __init__(self, item_type, default=None, allow_none=None,
-               sep=',', kv_sep=':'):
+               sep=',', kv_sep=':', nest_pair='{}'):
     self.item_type = item_type
     self.sep = sep
     self.kv_sep = kv_sep
+    self.nest_pair = nest_pair
+    self.is_sub_value = type(item_type) in (Value, ValueList, ValueDict)
+    if self.is_sub_value:
+      self.sub_nest_pair = item_type.nest_pair
+    else:
+      self.sub_nest_pair = None
     if allow_none is None:
       self.allow_none = item_type is str
     else:
@@ -101,18 +151,28 @@ class ValueDict(object):
     elif type(val) is str:
       # convert str to dict
       d = {}
-      kvs = val.split(self.sep)
+      kvs = split_nest(val, self.sep, self.sub_nest_pair, True)
       for kv in kvs:
-        key, val = kv.split(self.kv_sep)
+        key, val = split_nest(kv, self.kv_sep, self.sub_nest_pair)
         d[key] = val
       val = d
+    elif type(val) in (list, tuple):
+      # allow list of entries and merge them
+      res = {}
+      for elem in val:
+        d = self.parse(elem)
+        res.update(d)
+      return res
     elif type(val) is not dict:
       raise ValueError("expected dict: %s" % val)
     # rebuild dict
     res = {}
     for key in val:
       v = val[key]
-      r = parse_scalar(self.item_type, v, self.allow_none)
+      if self.is_sub_value:
+        r = self.item_type.parse(v)
+      else:
+        r = parse_scalar(self.item_type, v, self.allow_none)
       res[key] = r
     return res
 
--- test/unit/cfgcore_value.py
+++ test/unit/cfgcore_value.py
@@ -9,7 +9,7 @@ def config_value_parse_str_test():
   assert parse_scalar(str, None, True) is None
 
 
-def config_valuse_parse_int_test():
+def config_value_parse_int_test():
   assert parse_scalar(int, 12) == 12
   assert parse_scalar(int, "12") == 12
   with pytest.raises(ValueError):
@@ -17,7 +17,7 @@ def config_valuse_parse_int_test():
   assert parse_scalar(int, None, True) is None
 
 
-def config_valuse_parse_bool_test():
+def config_value_parse_bool_test():
   assert parse_scalar(bool, True)
   assert parse_scalar(bool, "On")
   assert parse_scalar(bool, "TRUE")
@@ -26,3 +26,80 @@ def config_valuse_parse_bool_test():
   with pytest.raises(ValueError):
     parse_scalar(bool, None)
   assert parse_scalar(bool, None, True) is None
+
+
+def config_value_split_nest_test():
+  assert split_nest("") == []
+  assert split_nest("bla") == ["bla"]
+  assert split_nest("foo,bar") == ["foo", "bar"]
+  assert split_nest("foo,(bar,baz)") == ["foo", "bar,baz"]
+  assert split_nest("foo,(bar,(baz,foo))") == ["foo", "bar,(baz,foo)"]
+  assert split_nest("foo,{bar,(baz,foo)}", nest_pair='{}') == [
+      "foo", "bar,(baz,foo)"]
+
+
+def config_value_list_test():
+  l = ValueList(str)
+  assert l.parse("bla") == ["bla"]
+  assert l.parse("a,b") == ["a", "b"]
+  assert l.parse(["a", "b"]) == ["a", "b"]
+
+
+def config_value_list_nest_value_test():
+  l = ValueList(Value(str))
+  assert l.parse("bla") == ["bla"]
+  assert l.parse("a,b") == ["a", "b"]
+  assert l.parse(["a", "b"]) == ["a", "b"]
+
+
+def config_value_list_nest_list_test():
+  l = ValueList(ValueList(str, sep='+'))
+  assert l.parse("bla") == [["bla"]]
+  assert l.parse("a,b") == [["a"], ["b"]]
+  assert l.parse("a+b") == [["a", "b"]]
+  assert l.parse(["a", "b"]) == [["a"], ["b"]]
+  assert l.parse([["a"], ["b"]]) == [["a"], ["b"]]
+  l = ValueList(ValueList(str))
+  assert l.parse("(a,b)") == [["a", "b"]]
+
+
+def config_value_list_nest_dict_rest():
+  l = ValueList(ValueDict(str, sep='+'))
+  assert l.parse("a:b") == [{'a': 'b'}]
+  assert l.parse("a:b,c:d") == [{'a': 'b'}, {'c': 'd'}]
+  assert l.parse("a:b+c:d,e:f") == [{'a': 'b', 'c': 'd'}, {'e': 'f'}]
+  l = ValueList(ValueDict(str))
+  assert l.parse("{a:b,c:d},e:f") == [{'a': 'b', 'c': 'd'}, {'e': 'f'}]
+
+
+def config_value_dict_test():
+  d = ValueDict(str)
+  assert d.parse("a:b") == {'a': 'b'}
+  assert d.parse("a:b,c:d") == {'a': 'b', 'c': 'd'}
+  assert d.parse({'a': 'b', 'c': 'd'}) == {'a': 'b', 'c': 'd'}
+  # allow list of partial dicts
+  assert d.parse(['a:b', 'c:d']) == {'a': 'b', 'c': 'd'}
+  assert d.parse(['a:b', {'c':'d'}]) == {'a': 'b', 'c': 'd'}
+  # last one wins in list
+  assert d.parse(['a:b', 'a:d']) == {'a': 'd'}
+
+
+def config_value_dict_nest_value_test():
+  d = ValueDict(Value(str))
+  assert d.parse("a:b") == {'a': 'b'}
+  assert d.parse("a:b,c:d") == {'a': 'b', 'c': 'd'}
+  assert d.parse({'a': 'b', 'c': 'd'}) == {'a': 'b', 'c': 'd'}
+
+
+def config_value_dict_nest_list_test():
+  d = ValueDict(ValueList(str, sep='+'))
+  assert d.parse("a:b") == {'a': ['b']}
+  assert d.parse("a:b+c") == {'a': ['b', 'c']}
+  assert d.parse({'a': ['b', 'c']}) == {'a': ['b', 'c']}
+  d = ValueDict(ValueList(str))
+  assert d.parse("a:(b,c)") == {'a': ['b', 'c']}
+
+
+def config_value_dict_nest_dict_test():
+  d = ValueDict(ValueDict(str))
+  assert d.parse("a:{b:c}") == {'a': {'b': 'c'}}
