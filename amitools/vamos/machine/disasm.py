857d6d205638444fb55694ef0255ee7382f53486
Christian Vogelgsang <chris@vogelgsang.org>
2019-12-22 21:00:09 +0100

    added code disasm for machine

--- amitools/vamos/machine/disasm.py
+++ amitools/vamos/machine/disasm.py
@@ -16,6 +16,38 @@ class DisAsm(object):
     num_bytes, txt = self.cpu.disassemble_raw(pc, data)
     return num_bytes, txt
 
+  def disassemble_line(self, pc, data):
+    """disassemble a line and return (pc, words, code)"""
+    num_bytes, txt = self.disassemble_raw(pc, data)
+    words = []
+    pos = 0
+    num_words = num_bytes // 2
+    for i in range(num_words):
+      w = data[pos] << 8 | data[pos+1]
+      words.append(w)
+      pos += 2
+    return (pc, words, txt)
+
+  def disassemble_block(self, data, start_pc=0):
+    """disassemble a block and return a list of (pc, words, code)"""
+    num = len(data)
+    off = 0
+    pc = start_pc
+    code = []
+    while off < num:
+      pc, words, txt = self.disassemble_line(pc, data[off:])
+      code.append((pc, words, txt))
+      num_bytes = len(words) * 2
+      off += num_bytes
+      pc += num_bytes
+    return code
+
+  def dump_block(self, code, func=print):
+    """dump a code block"""
+    for pc, words, txt in code:
+      words_str = " ".join(["%04x" % x for x in words])
+      func("%08x:  %-20s  %s" % (pc, words_str, txt))
+
   def _parse_trap(self, tid):
     func = self.traps.get_func(tid)
     if func:
