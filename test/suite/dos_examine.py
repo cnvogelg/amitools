ef3cc4dc9e3c84745236dc0d7c714d01b007ed6d
Christian Vogelgsang <chris@vogelgsang.org>
2021-01-11 21:12:41 +0100

    fixed test

--- test/suite/dos_examine.py
+++ test/suite/dos_examine.py
@@ -4,12 +4,12 @@ import pytest
 
 def dos_examine_test(vamos, tmpdir):
     test_dir = tmpdir / "bla"
-    os.mkdir(test_dir)
+    os.mkdir(str(test_dir))
     # create some content
-    fh = open(test_dir / "foo", "w")
+    fh = open(str(test_dir / "foo"), "w")
     fh.write("hello, world!\n")
     fh.close()
-    os.mkdir(test_dir / "bar")
+    os.mkdir(str(test_dir / "bar"))
     # show dir
     rc, stdout, stderr = vamos.run_prog("dos_examine", "root:" + str(test_dir)[1:])
     assert rc == 0
